568a394dd815e65019dac2a8bfa83f3aa45cdc86
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2024-03-15 17:12:27 -0700

    feat: Started database propperly

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import apiRouter from './src/routes';
+import connectionDB from './src/connection/connection';
 
 const app: Express = express();
 const morgan = require('morgan');
 const port = 3000;
+
 
 app.use(morgan('dev'));
 
@@ -11,6 +13,9 @@ app.use(express.json());
 
 app.use(apiRouter);
 
+
+connectionDB();
+
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
